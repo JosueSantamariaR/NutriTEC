0cc1f8359e092b04a3c72cef838b985841f9a1b1
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2021-10-31 21:53:39 -0600

    Database  Changes

---
+++ DataBase.pl
@@ -0,0 +1,16 @@
+%dietas
+diet([hipocalorica|S],S).
+diet([hipercalorica|S],S).
+diet([detox|S],S).
+diet([proteica|S],S).
+diet([vegetariana|S],S).
+diet([paleo|S],S).
+diet([alcalina|S],S).
+diet([mediterranea|S],S).%enfermedades cardiovasculares
+diet([vegana|S],S).
+diet([keto|S],S).
+
+Padecimientos [[Dislipidemia“Problemas del control del colesterol” “Dieta baja en grasas”]
+Padecimientos [Hipercolesterolemia “aumento de los niveles considerados normales de colesterol en la sangre” “Dieta vegana”]
+NivelesActividad [Inicial (0-2 veces)|Intermedio (3-4 veces)|Avanzado (5 ó más veces)]
+Dietas [NombreDieta TipoDieta Calorías[padecimientos NO recomendados] [padecimiento recomendados] [Actividades NO recomendadas][Actividades recomendadas] [Detalle Dieta]]
