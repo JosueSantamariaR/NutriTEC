0a5a710aef3a1fcf3b80021c3d719cf07457e7df
Carlos Akión Garro Campos <carloscamp1008@estudianTEC.cr>
2021-11-01 00:36:17 -0600

    Detox

--- DataBase.pl
+++ DataBase.pl
@@ -1,40 +1,37 @@
 %dietas
-diet([hipocalorica|S],S).   % sobrepeso u obesidad  diabetes, el colesterol o la hipertensiï¿½n arterial
-
+diet([hipocalorica|S],S).   % sobrepeso u obesidad  diabetes, el colesterol o la hipertensi?n arterial
 diet([hipercalorica|S],S).  % desnutricion - subir masa muscular
+diet([detox|S],S).   % trastornos del sistema digestivo, enfermedades autoinmunes, fibromialgia, síndrome de fatiga crónica y la tan temida inflamación
 
 
 
-
-
-% elimina grasas y azucares, no se puede cocinar fritos
-% se permite la comida hervida y a la plancha
 
 
 
 % hipocalorica 1  a 1200 calorias
-
+% elimina grasas y azucares, no se puede cocinar fritos
+% se permite la comida hervida y a la plancha
 Desayuno
 1 vaso de leche desnatada.
 40 g de cereales.
-Cafï¿½ solo o infusiï¿½n
+Cafe solo o infusion
 
 Merienda medio
 20 g de pan.
-60 g de jamï¿½n york.
-Cafï¿½ o infusiï¿½n
+60 g de jam?n york.
+Cafe o infusion
 
 Almuerzo
 150 g de coliflor hervida.
 75 g de pollo asado.
 100 g de patatas.
 20 g de pan.
-200 g de piï¿½a natural.
+200 g de pinia natural.
 
 Merienda tarde
 20 g de pan
 50 g de queso.
-Cafï¿½ o infusiï¿½n.
+Cafe o infusion.
 
 Cena
 15 g de pasta
@@ -48,34 +45,34 @@ Cena
 
 
 
-% hipocalorica 2  a 1200 calorias
+% hipocalorica 2  a 1500 calorias
 
 Desayuno
 2 yogures desnatados.
-30 g de galletas.
-Cafï¿½ o infusiï¿½n.
+50 g de galletas.
+Cafe o infusion.
 
 Merienda medio
-15 g de biscotes.
-40 g de queso fresco.
-Cafï¿½ o infusiï¿½n.
+50 g de biscotes.
+100 g de queso fresco.
+Cafe o infusion.
 
 Almuerzo
 150 g de verduras.
-75 g de albï¿½ndigas.
-30 g de arroz.
-20 g de pan.
+100 g de albondigas.
+50 g de arroz.
+50 g de pan.
 200 g de fresas.
 
 Merienda tarde
-15 g de galletas.
-Cafï¿½ con 100 ml de leche desnatada.
+25 g de galletas.
+Cafe con 100 ml de leche desnatada.
 
 Cena
 150 g de lechuga
 150 g de tomate.
 100 g de patatas
-1 huevo
+3 huevo
 200 g de naranja.
 
 
@@ -87,23 +84,23 @@ Desayuno
 60 g de cereales integrales.
 1 yogurt.
 2 piezas de fruta.
-Cafï¿½ solo o infusiï¿½n.
+Cafe solo o infusion.
 
 
 Merienda medio
 60 g de pan.
-90 g de jamï¿½n york.
+90 g de jam?n york.
 150 g de tomate.
-Cafï¿½ o infusiï¿½n con 100 ml de leche.
+Cafe o infusion con 100 ml de leche.
 
 
 Almuerzo
 300 g de coliflor hervida
-15 g de jamï¿½n serrano en tacos.
+15 g de jamon serrano en tacos.
 150 g de pollo asado.
 150 g de patatas.
 60 g de pan.
-400 g de melï¿½n.
+400 g de melon.
 
 
 Merienda tarde
@@ -111,15 +108,15 @@ Merienda tarde
 75 g de nueces
 160 g de manzana
 200 ml de leche.
-Cafï¿½ solo o infusiï¿½n.
+Cafe solo o infusion.
 
 
 Cena
 300 g de verduras
 50 g de guisantes
-35 g de requesï¿½n.
+35 g de requeson.
 150 g de sardinas a la plancha.
-150 g de espï¿½rragos
+150 g de esparragos
 100 g de patatas cocidas.
 40 g de pan.
 300 g de mandarinas.
@@ -130,7 +127,7 @@ Desayuno
 2 yogures desnatados.
 100 g de galletas.
 3 piezas de fruta.
-Cafï¿½ con 100 ml de leche.
+Cafe con 100 ml de leche.
 
 
 Merienda medio
@@ -138,7 +135,7 @@ Merienda medio
 100 g de almendras.
 100 g de zanahoria cruda.
 2 yogurt.
-Cafï¿½ solo o infusiï¿½n
+Cafe solo o infusion
 
 
 
@@ -154,18 +151,89 @@ Almuerzo
 
 Merienda tarde
 100 g de pan.
-100 g de jamï¿½n york.
-Cafï¿½ solo o infusiï¿½n con 200 ml de leche.
-2 pieza de fruta pequeï¿½a.
+100 g de jamon york.
+Cafe solo o infusion con 200 ml de leche.
+2 pieza de fruta peq.
 
 
 
 Cena
 250 g de alcachofas.
-250 g de salmï¿½n a la plancha.
-150 g de purï¿½ de patatas (con 100 ml de leche).
+250 g de salmon a la plancha.
+150 g de pure de patatas (con 100 ml de leche).
 100 g de pan.
 240 g de manzana.
+
+
+
+%detox 1 1400 calorias
+
+Desayuno
+Te verde.
+Avena con leche de soja.
+Arandanos.
+
+
+
+
+Merienda medio
+tomate natural con perejil.
+
+
+Almuerzo
+Alcachofas al vapor
+Batido de apio y piña
+infusion te verde
+
+
+
+Merienda tarde
+zumo de naranja y piña
+
+
+
+
+Cena
+Batido detox.
+1 apio.
+1 tomate.
+1 pepino.
+1L de agua.
+zumo de arandano .
+
+
+
+%detox 2 1600 calorias
+Desayuno
+Te verde.
+30g de pan con tomate y aceite de oliva
+Arandanos.
+
+
+
+Merienda medio
+te verde.
+10 almendras.
+
+
+Almuerzo
+Ensalada de espinacas con ajo.
+semillas de mostaza al vapor.
+merluza a la plancha .
+zumo de naranja.
+
+
+
+Merienda tarde
+papaya y piña.
+
+Cena
+Batido detox.
+3 pepinos.
+1 piña.
+semillas de sesamo.
+3 vasos de agua.
+zumo de uvas rojas.
 
 
 Keto 1
@@ -183,22 +251,22 @@ Media cucharada de leche de almendra
 
 Merienda Media
 chips de parmesano
-1 triÃ¡ngulo de queso parmesano por raciÃ³n
+1 triángulo de queso parmesano por ración
 Albahaca
 Sal
 Pimienta
 
 Almuerzo
-salmÃ³n con brÃ³coli y kale
-1 filete de salmÃ³n
-100 gr de brÃ³coli
+salmón con brócoli y kale
+1 filete de salmón
+100 gr de brócoli
 3 cucharadas de almendras
 Pimienta
 Nuez moscada
 Canela
-2 puÃ±ados de kale
+2 puñados de kale
 Merienda Media
-100 gr de cecina o jamÃ³n
+100 gr de cecina o jamón
 
 Cena
 ramen de ternera
@@ -213,12 +281,12 @@ Caldo de huesos
 Keto 2
 Desayuno 
 250 gr de yogur de coco
-1 cucharada de proteÃ­na
+1 cucharada de proteína
 30 gr de almendras
 20 gr de mantequilla
 
 Merienda Media
-100 gr de cecina o jamÃ³n
+100 gr de cecina o jamón
 
 Almuerzo
 Ensalada de pastrami
@@ -228,18 +296,18 @@ Ensalada de pastrami
 50 gr de crema de queso
 
 Merienda Media
-brÃ³coli picante
-80 gr de brÃ³coli asado
+brócoli picante
+80 gr de brócoli asado
 10 gr de chile cortados en trozos y asados
 
 Cena
 gambas con arroz de coliflor
 100 gr de coliflor
 1 cucharada de aceite de coco
-50 gr de judÃ­as verdes
+50 gr de judías verdes
 150 gr de gambas
 50 gr de crema de queso
-1 puÃ±ado de cacahuetes machacados
+1 puñado de cacahuetes machacados
 10 gr de cebollino
 
 
@@ -254,7 +322,7 @@ diet([mediterranea|S],S). %enfermedades cardiovasculares
 diet([vegana|S],S).
 diet([keto|S],S).%no diabeticos, bajar de peso
 
-Padecimientos [[Dislipidemiaï¿½Problemas del control del colesterolï¿½ ï¿½Dieta baja en grasasï¿½]
-Padecimientos [Hipercolesterolemia ï¿½aumento de los niveles considerados normales de colesterol en la sangreï¿½ ï¿½Dieta veganaï¿½]
-NivelesActividad [Inicial (0-2 veces)|Intermedio (3-4 veces)|Avanzado (5 ï¿½ mï¿½s veces)]
-Dietas [NombreDieta TipoDieta Calorï¿½as[padecimientos NO recomendados] [padecimiento recomendados] [Actividades NO recomendadas][Actividades recomendadas] [Detalle Dieta]]
+Padecimientos [[Dislipidemia?Problemas del control del colesterol? ?Dieta baja en grasas?]
+Padecimientos [Hipercolesterolemia ?aumento de los niveles considerados normales de colesterol en la sangre? ?Dieta vegana?]
+NivelesActividad [Inicial (0-2 veces)|Intermedio (3-4 veces)|Avanzado (5 ? m?s veces)]
+Dietas [NombreDieta TipoDieta Calor?as[padecimientos NO recomendados] [padecimiento recomendados] [Actividades NO recomendadas][Actividades recomendadas] [Detalle Dieta]]
