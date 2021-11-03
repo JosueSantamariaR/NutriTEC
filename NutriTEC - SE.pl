f322723fc99b6115155a8ef6b4378f183f722d74
Josue Santamaria <48070852+Santamix728@users.noreply.github.com>
2021-11-03 13:25:11 -0600

    Comparaciones

--- NutriTEC - SE.pl
+++ NutriTEC - SE.pl
@@ -1,71 +1,49 @@
-% Se importan los archivos que contienen la base de datos y el BNF
+% Importación de los archivos que contienen la base de datos y el BNF
+
 :-consult('NutriTEC - BNF').
 :-consult('NutriTEC - DataBase').
 :-style_check(-singleton).
 
 
-%******************************************************************************
-%***********************    Operaciones basicas    ****************************
-%******************************************************************************
-
-% Descripción		:	Se verifica si una lista esta vacia
-% Nombre de Hecho	:	lista_vacia([],X)
-% Parámetro			:	lista a verificar, resultado
-% Salida            :   boolean que indica si la lista esta vacia o no
+%Función para verificar si una lista se encuentra vacía.
 lista_vacia(List, Empty) :-
 	length(List, Len),
 	(   Len =< 1
 	->  Empty = true
-	;   Empty = false
-	).
+	;   Empty = false).
 
-% Descripción		:	Se convierte el parametro a lista
-% Nombre de Hecho	:	input_to_list(X)
-% Parámetro			:	resultado
-% Salida            :   entrada convertida en lista
+
+%Recibe un parametro para introducirlo en una lista.
 input_to_list(L):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs),
 	atomic_list_concat(L,' ',A).
 
-% Descripción		:	Se convierte la entrada a string
-% Nombre de Hecho	:	input_to_string(X)
-% Parámetro			:	resultado
-% Salida            :   entrada convertida en string
+
+%Función para convertir el parametro en un string.
 input_to_string(A):-
 	read_line_to_codes(user_input,Cs),
 	atom_codes(A,Cs).
 
-% Descripción		:	Se convierte la entrada a string
-% Nombre de Hecho	:	list_to_string([],X)
-% Parámetro			:	lista, resultado
-% Salida            :   lista ingresada convertida en string
+
+%Función que convierte una lista en string.
 list_to_string(List, String):-
 	atomic_list_concat(List, ' ', String).
 
-% Descripción		:	Se convierte la entrada a lista
-% Nombre de Hecho	:	string_to_list_of_atoms(X,[])
-% Parámetro			:	string, resultado
-% Salida            :   entrada convertida en lista
+
+%Función para convertir una entrada en lista.
 string_to_list_of_atoms(X,L):-
 	atom_codes(X,A),
 	atomic_list_concat(L,' ',X).
 
-% Descripción		:	Se verifica si un elemento pertenece a la lista ingresada
-% Nombre de Hecho	:	miembro(X,[])
-% Parámetro			:	elemento, lista
-% Salida            :   boolean que indica si el elemento pertenece a la lista dada
+
+%Verifica si la entrada pertenece a la lista. 
 miembro(X,[X|_]).
 miembro(X,[_|T]):-miembro(X,T).
 
-%******************************************************************************
-%******************************************************************************
-%******************************************************************************
 
-% Descripción		:	Se busca la palabra de inicio
-% Nombre de Hecho	:	compareInicio([])
-% Parámetro			:	lista oracion
-% Salida            :   llamar a la funcion inicio
+%Función de Busqueda de palabras de inicio.
+%Realiza una busqueda de las posibles palabras con las que se puede iniciar 
 checkerStart([]):- input_to_list(Oracion1),
 	checkerStart(Oracion1).
 checkerStart([H|_]):- listStart(L),
@@ -75,75 +53,146 @@ checkerStart([H|T], X):- listStart(L),
 	\+miembro(H,L),
 	compareInicio(T,X).
 
-% Descripción		:	Se obtiene la lista de comidas disponibles
-% Nombre de Hecho	:	listStart(L)
-% Parámetro			:	resultado
-% Salida            :   lista de comidas disponibles
+
+%Busca las palabras dentro de nuestra deficnicón de elementos que inician la conversación.
 listStart(L) :- findall(X, (start([X|_])), L).
 
+comparePadecimientos([],X):- nl, writeln('No se encuentra el padecimiento'),
+              writeln('Intente de nuevo'),nl,
+              input_to_list(Oracion),
+              comparePadecimientos(Oracion,X).
+comparePadecimientos([H|_], X):- listaTipoDePadecimientos(L),
+                miembro(H,L),
+                X = H, !.
+comparePadecimientos([H|T], X):- listaTipoDePadecimientos(L),
+                            \+miembro(H,L),
+                            comparePadecimientos(T,X).
+listaTipoDePadecimientos(L) :- findall(X, (diet([_,_,_,_,_,X,_,_,_,_])) , L).
 
-%******************************************************************************
-%******************************************************************************
-%******************************************************************************
 
-% Descripción		:	Se piden los datos al usuario
-% Nombre de Hecho	:	ingresarDatos(NombreDieta,Padecimientos,Calorias,Actividad,Frecuencia,TipoDieta,MenuDieta)
-% Parámetro			:	-------------
-% Salida            :   
+
+
+
+
+compareCalorias([],X):- nl, writeln('No hay dietas con este tipo de calorias'),
+          writeln('Intente de nuevo'),nl,
+          input_to_list(Oracion),
+          compareCalorias(Oracion,X).
+compareCalorias([H|_], X):- listaCalorias(L),
+            miembro(H,L),
+            X = H, !.
+compareCalorias([H|T], X):- listaCalorias(L),
+                      \+miembro(H,L),
+                      compareCalorias(T,X).
+
+listaCalorias(L) :- findall(X, (diet([_,_,X,_,_,_,_,_,_,_])) , L).
+
+
+
+
+
+
+compareActividad([],X):- nl, writeln('La actividad no se encuentra'),
+            writeln('Intente de nuevo'),nl,
+            input_to_list(Oracion),
+            compareActividad(Oracion,X).
+compareActividad([H|_], X):- listaActividad(L),
+              miembro(H,L),
+              X = H, !.
+compareActividad([H|T], X):- listaActividad(L),
+                        \+miembro(H,L),
+                        compareActividad(T,X).
+
+listaActividad(L) :- findall(X, (diet([_,_,_,_,_,_,X,_,_,_])) , L).
+
+
+
+
+compareFrecuencia([],X):- nl, writeln('La frecuencia no corresponde'),
+            writeln('Intente de nuevo'),nl,
+            input_to_list(Oracion),
+            compareFrecuencia(Oracion,X).
+compareFrecuencia([H|_], X):- listaFrecuencia(L),
+              miembro(H,L),
+              X = H, !.
+compareFrecuencia([H|T], X):- listaFrecuencia(L),
+                        \+miembro(H,L),
+                        compareFrecuencia(T,X).
+
+listaFrecuencia(L) :- findall(X, (diet([_,_,_,X,_,_,_,_,_,_])) , L).
+
+
+
+
+
+compareTipoDieta([],X):- nl, writeln('El tipo de dieta no corresponde'),
+            writeln('Intente de nuevo'),nl,
+            input_to_list(Oracion),
+            compareTipoDieta(Oracion,X).
+compareTipoDieta([H|_], X):- listaTipoDieta(L),
+              miembro(H,L),
+              X = H, !.
+compareTipoDieta([H|T], X):- listaTipoDieta(L),
+                        \+miembro(H,L),
+                        compareTipoDieta(T,X).
+
+listaTipoDieta(L) :- findall(X, (diet([_,X,_,_,_,_,_,_,_,_])) , L).
+
+%Función de ingreso y lectura de los datos que introduce el usuario.
 ingresarDatos(NombreDieta,Padecimientos,Calorias,Actividad,Frecuencia,TipoDieta,MenuDieta):-
+
 	% Se busca la enfermedad
 	nl, writeln('¿Tienes alguna enfermedad por la que ha iniciado este proceso?'),
-	input_to_list(Oracion1),
-	validacion_gramatical(Oracion1),
-	comparePadecimientos(Oracion1, PadTemp),
+	input_to_list(Oracion1),%Se ingresa lo introducido por el usuario a una lista para compararlo.
+	validacion_gramatical(Oracion1), %Se valida que la oración sea gramaticalmente correcta.
+	comparePadecimientos(Oracion1, PadTemp), %Llama a la función para buscar los padecimientos relacionados.
 	Padecimientos = PadTemp,
+
 
 	% Se buscan las calorias
 	nl, write('¿Tienes pensado una cantidad específica de calorías diarias por consumir?'), nl,
 	input_to_list(Oracion2),
 	validacion_gramatical(Oracion2),
-	compareKcal(Oracion2, KcalTemp),
+	compareKcal(Oracion2, KcalTemp), %Llama a la función para comparar con respecto a las calorías de las dietas.
 	Calorias = KcalTemp,
+
 
 	% Se busca tipo actividad fisica
 	nl, writeln('¿Practicas algún deporte?'),
 	input_to_list(Oracion3),
 	validacion_gramatical(Oracion3),
-	compareActividad(Oracion3, ActividadTemp),
+	compareActividad(Oracion3, ActividadTemp), %Llama a la función para comparar con respecto a las actividades que realiza.
 	Actividad = ActividadTemp,
+
 
 	% Se busca frecuencia actividad fisica
 	nl, writeln('¿Eres activo físicamente?'),
 	input_to_list(Oracion4),
 	validacion_gramatical(Oracion4),
-	compareFrecuencia(Oracion4, FrecuenciaTemp),
+	compareFrecuencia(Oracion4, FrecuenciaTemp), %Llama a la función para comparar con respecto a la frecuencia de su actividad.
 	Frecuencia = FrecuenciaTemp,
+
 
 	% Se busca el tipo de dieta
 	nl, write('¿Tienes un tipo de dieta te gustaría realizar?'), nl,
 	input_to_list(Oracion5),
 	validacion_gramatical(Oracion5),
-	compareTipoDieta(Oracion5, TipoDietaTemp),
+	compareTipoDieta(Oracion5, TipoDietaTemp), %Llama a la función para buscar el tipo de dietas según lo obtenido.
 	TipoDieta = TipoDietaTemp,
+
 
 	% Se validan los datos y se busca la referencia en caso de existir
 	buscarDietaConDatosIngresados(NombreTemp, Padecimientos, Calorias, Actividad, Frecuencia, TipoDieta, MenuDietaTemp).
 
 
-% Descripción   : Se verifican los datos ingresados por el usuario
-% Nombre de Hecho : verificarDatos(NombreRest, NombreIngresado, NombreIngresado2, SaborComida, SaboresDisponibles)
-% Parámetro     : NombreRestaurante, NombreIngresado, NombreIngresado2, SaborComida, SaboresDisponibles
-% Salida            :   boolean que indica si los datos se encuentran en la base de datos o no
+%Función para verificar que los datos ingresados por el usuario se encuentren en la base de datos.
 verificarDatos(NombreDieta, NombreIngresado, NombreIngresado2, MenuDieta, MenuDietaDisponibles):-
 	miembro(NombreDieta, NombreIngresado),
 	miembro(NombreDieta, NombreIngresado2),
 	miembro(MenuDieta, MenuDietaDisponibles).
 
 
-% Descripción   : Busca el mejor restaurante segun los parametros dados.
-% Nombre de Hecho : buscarRestauranteConDatosIngresados(NombreRest, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada)
-% Parámetro     : NombreRestaurante, TipoMenu, TipoComida, SaborComida, TipoBebida, LugarDeseado, CantidadDeseada
-% Salida            :   referencia generada si se encuentran los datos ingresados
+%Realiza una busqueda de la dietaa recomendar según los datos que obtuvo del usuario.
 buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecuencia,TipoDieta,MenuDieta):-
 	diet([NombreDieta,TipoDieta,Calorias,[Padecimientos],[Padecimientos],[Actividad],[Actividad], [MenuDieta]]),
 	padecimientos([NombreIngresado]),
@@ -152,6 +201,7 @@ buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecu
 	crearReferencia(NombreDieta,MenuDieta),
 	buscarNuevamente(), !.
 
+
 buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecuencia,TipoDieta,MenuDieta):-
 	diet([NombreDieta,TipoDieta,Calorias,[Padecimientos],[Padecimientos],[Actividad],[Actividad], [MenuDieta]]),
 	write('Intente de nuevo'), nl, nl,
@@ -159,6 +209,8 @@ buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecu
 	validacion_gramatical(Oracion),
 	buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecuencia,TipoDieta,MenuDieta).
 
+
+%Si no hay comprobaciones verdaderas tira un mensaje de error.
 buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecuencia,TipoDieta,MenuDieta):-
 	diet([NombreDieta,TipoDieta,Calorias,[Padecimientos],[Padecimientos],[Actividad],[Actividad], [MenuDieta]]),
 	+verificarDatos(NombreDieta, NombreIngresado, NombreIngresado2, MenuDieta, MenuDietaDisponibles),
@@ -167,10 +219,8 @@ buscarDietaConDatosIngresados(NombreDieta,Padecimientos,Calorias,Actividad,Frecu
 	nl, writeln('**'), nl,
 	buscarNuevamente(), !.
 
-% Descripción   : Se crea la referencia para el mejor restaurante segun los parametros dados.
-% Nombre de Hecho : crearReferencia(NombreRest, DireccionTemp, Obligaciones)
-% Parámetro     : NombreRestaurante, DireccionTemporal, Obligaciones
-% Salida            :   referencia generada si se encuentran los datos ingresados
+
+%Crea una referencia a la dieta que se va entregar y la muestra según los resultados obtenidos.
 crearReferencia(NombreDieta, Calorias,MenuDieta):-
 	nl, writeln('**'), nl,
 	write('--> Nuestra sugerencia de dieta es: '), write(NombreDieta), writeln(' <--'),
@@ -179,10 +229,7 @@ crearReferencia(NombreDieta, Calorias,MenuDieta):-
 	nl, writeln('**'), nl.
 
 
-% Descripción   : Se pregunta al usuario si desea buscar nuevamente.
-% Nombre de Hecho : buscarNuevamenteAux(Respuesta)
-% Parámetro     : respuesta del usuario si desea buscar nuevamente o no
-% Salida            :   mensaje de despedida en caso de no querer volver a buscar
+%En caso de no encontrar comprobaciones pregunta al usuario si desea buscar nuevamente.
 buscarNuevamenteAux(Respuesta):- miembro('si',Respuesta),
 	ingresarDatos(A,B,C,D,E,F,G).
 buscarNuevamenteAux(Respuesta):- +miembro('si',Respuesta),
@@ -192,10 +239,7 @@ buscarNuevamente():- nl, writeln('--> Desea buscar nuevamente? (Debe responder c
 	buscarNuevamenteAux(Respuesta), !.
 
 
-% Descripción		:	Regla principal que ejecuta el programa.
-% Nombre de Hecho	:	inicio()
-% Parámetro			:	-
-% Salida            :   -
+%Función principal para la ejecución del programa.
 inicio():-
 	input_to_list(Oracion),
 	checkerStart(Oracion).
